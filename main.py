2d090b56d881fe2965013aae0a32bf815e3296b2
alphadzingina <alphaadx6@gmail.com>
2022-05-15 10:00:43 +0100

    convert dataframe to dictionary in desired format

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import pandas
 {"A": "Alfa", "B": "Bravo"}
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#print(data)
+
+phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+#print(phonetic_dict)
+
 
 
 
